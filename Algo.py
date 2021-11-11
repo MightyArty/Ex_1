aacb4251b350ed4840fca16dbb1dfc9887491ebd
liorpatael <Lior13313>
2021-11-11 16:22:00 +0200

    try

--- Algo.py
+++ Algo.py
@@ -2,7 +2,27 @@ import Building
 import Elevators
 import Calls
 import json
+import math as m
+
 
 class Algo:
-    def __timeTo__(self,call,elevator):
-        x = 4
+
+    def __init__(self,building,numOfElev):
+        self.building=Building('B5.json',"r")
+        self.numOfElev=building.size
+
+
+    def __timeTodest__(self, call, elevator):
+        floorsTime = m.abs(call.dest - call.src) / elevator.speed
+        return floorsTime + elevator.minFloor + elevator.maxFloor + elevator.closeTime + elevator.openTime
+
+    def allocate(self,call):
+        num=self.numOfElev
+        if(num<1):return None
+        elif num==1:
+            return 0
+            if(num>1):
+
+
+
+    def range(self,data):
--- Building.py
+++ Building.py
@@ -4,11 +4,8 @@ from Elevators import Elevators
 
 class Building:
     def __init__(self, data):
-            self._maxFloor = int(data["_maxFloor"])
-            self._minFloor = int(data["_minFloor"])
-            self.elevators = []
-            self.size = len(data["_elevators"])
-            for i in data["_elevators"]:
-                self.elevators.append(Elevators(i))
-
+        data=input("Enter the file name")
+        open(data,"r")
+        self._maxFloor = int(data["_maxFloor"])
+        self._minFloor = int(data["_minFloor"])
 
