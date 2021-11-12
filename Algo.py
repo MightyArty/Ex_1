9535009b625b6881846d7f97e0333bfcf8b16365
Artem Shabalin <tomshabalin95@gmail.com>
2021-11-12 16:45:38 +0200

    classes updated

--- Algo.py
+++ Algo.py
@@ -2,11 +2,11 @@ import json
 import csv
 
 import Building
-from Calls import CallsList
+import Calls
 
 
 def __init__(self, calls, building, out):
-    self.callList = CallsList(calls)
+    self.callList = Calls(calls)
     self.out = out
     self.building = Building(building)
 
@@ -16,9 +16,9 @@ def timeTo(self, call):
     result = self._closeTime + self._startTime + (fromTo / self._speed) + self._stopTime + self._openTime
     return result
 
+def allocate(self):
+    if self.calls.src < self.building._minFloor or self.calls.src > self.building._maxFloor or self.calls.dest < self.building._minFloor or self.calls.dest > self.building._maxFloor:
+        print("The floor does not exist :(")
+    for i in Calls:
+        if self.calls.status != 0:
 
-# if __name__ == '__main__':
-#     with open("Ex1_Calls/Calls_a.csv") as file:
-#         reader = csv.reader(file)
-#         for i in reader:
-#             print(i)
