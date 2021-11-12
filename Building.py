e753091a0ed5617d6196b83aac68be2b987e5110
Artem Shabalin <tomshabalin95@gmail.com>
2021-11-12 15:48:17 +0200

    classes updated

--- Building.py
+++ Building.py
@@ -9,8 +9,8 @@ class Building:
             read = json.load(json_file)
             self._maxFloor = read["_maxFloor"]
             self._minFloor = read["_minFloor"]
-            line = read["_elevators"]
-            for line in line:
-                temp = Elevators(line["_id"], line["_speed"], line["_minFloor"], line["_maxFloor"], line["_closeTime"],
-                                 line["_openTime"], line["_startTime"], line["_stopTime"])
+            file = read["_elevators"]
+            for i in file:
+                temp = Elevators(i["_id"], i["_speed"], i["_minFloor"], i["_maxFloor"], i["_closeTime"],
+                                 i["_openTime"], i["_startTime"], i["_stopTime"])
                 self.elevArr.append(temp)
