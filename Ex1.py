b8c7a8dc56466830c9d166f971b08cb0fe76ebdb
liorpatael <Lior13313>
2021-11-16 15:23:37 +0200

    try

--- Ex1.py
+++ Ex1.py
@@ -5,6 +5,7 @@ import Building
 import Calls
 from Elevators import Elevators
 import node
+import ListCalls
 
 
 class Algo:
@@ -14,11 +15,13 @@ class Algo:
         self.building = Building(building)
         self.out = out
         self.Node = []
-        self.arr=[]
-        for elev in Building.Elevators:
-            self.Node.append(node(Building.Elevators.id))  # need to add the real id
-            self.arr[elev].append(building._elevators)
-        self.list=ListCalls(building.id)
+        self.arr = []
+        i = 0
+        for elev in building.elevArr:
+            self.Node.append(node(elev[i].id))
+            # self.arr.append(elev)
+            i = i + 1
+        self.list = ListCalls(building.id)
 
     # uploading the data to csv output file
     def saveToCSV(self, csv_file):
@@ -30,21 +33,43 @@ class Algo:
             with open(name, 'w', space='') as fp:
                 writer.writerow(cList)
 
-    def timeTo(self, call):
-        fromTo = abs(call.src - self._currentFloor) + abs(call.dest - call.src)
-        result = self._closeTime + self._startTime + (fromTo / self._speed) + self._stopTime + self._openTime
+    def timeToDest(self, n, elev, c):
+        fromTo = abs(n.src - n.dest)  # 1-2
+        result = elev.closeTime + elev.startTime + (fromTo / elev.speed) + elev.stopTime + elev.openTime
+        result += self.timeToSrc(n, elev, c.src)
         return result
+
+    def timeToSrc(self, n, elev, src):
+        fromTo = abs(n.src - src)  # 0-0
+        return elev.closeTime + elev.startTime + (fromTo / elev.speed) + elev.stopTime + elev.openTime
 
     def allocate(self, c):
         if self.calls.src < self.building._minFloor or self.calls.src > self.building._maxFloor or self.calls.dest < self.building._minFloor or self.calls.dest > self.building._maxFloor:
             print("The floor does not exist :(")
         tempTime = float('inf')
-        tempID= -1
-        for elev in range(len(self.building._elevators)):
-            time = self.calls.time + self.timeTo(c, elev)  # 4.37 + (dest - src) --> from 0 to -1
-            if tempTime > time:
-                tempTime = time
-                tempID = elev
+        tempID = -1
+        i = 0
+        index = 0
+        for elev in self.building.elevArr:
+            # checks whether the calls between the ranges
+            if self.isOn(self.Node[i].src, self.Node[i].dest, c.src):
+                time = c.time + self.timeToSrc(self.Node[i], elev, c.src)  # 4.37 + (dest - src) --> from 0 to -1
+                if time < tempTime:
+                    tempTime = time
+                    tempID = elev[i].id
+                    index = i
+            else:
+                # checks which elev will come first to the call src floor
+                time2 = c.time + self.timeToDest(self.Node[i], elev, c)
+                if time2 < tempTime:
+                    tempTime = time2
+                    tempID = elev[i].id
+                    index = i
+            i = i + 1
+        # modify the nodes
+        self.Node[index].src = c.src
+        self.Node[index].dest = c.dest
+        self.Node[index].time = c.time
         return tempID
 
         # for i in self.building._elevators:
